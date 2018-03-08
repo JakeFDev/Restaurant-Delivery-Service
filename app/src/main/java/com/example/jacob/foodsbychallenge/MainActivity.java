046cc3b3577f6d4091396e5688a25db5c50f9c80
Jacob Fulton <fulto069@d.umn.edu>
2018-03-08 14:43:47 -0600

    Added order status logic. Added new font. Layout changes

--- app/src/main/java/com/example/jacob/foodsbychallenge/MainActivity.java
+++ app/src/main/java/com/example/jacob/foodsbychallenge/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.jacob.foodsbychallenge;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.transition.TransitionManager;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -16,7 +20,12 @@ import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,22 +46,47 @@ public class MainActivity extends AppCompatActivity {
 
     public void onMonClick(View view) {
         setDeliveriesInView(Calendar.MONDAY);
+        resetAllButtons();
+        Button mondayBtn = findViewById(R.id.mondayBtn);
+        mondayBtn.setBackgroundResource(R.drawable.button_active);
+        mondayBtn.setTextColor(Color.WHITE);
+        mondayBtn.setElevation(100);
     }
 
     public void onTueClick(View view) {
         setDeliveriesInView(Calendar.TUESDAY);
+        resetAllButtons();
+        Button tuesBtn = findViewById(R.id.tuesdayBtn);
+        tuesBtn.setBackgroundResource(R.drawable.button_active);
+        tuesBtn.setTextColor(Color.WHITE);
+        tuesBtn.setElevation(100);
     }
 
     public void onWedClick(View view) {
         setDeliveriesInView(Calendar.WEDNESDAY);
+        resetAllButtons();
+        Button wedBtn = findViewById(R.id.wednesdayBtn);
+        wedBtn.setBackgroundResource(R.drawable.button_active);
+        wedBtn.setTextColor(Color.WHITE);
+        wedBtn.setElevation(100);
     }
 
     public void onThuClick(View view) {
         setDeliveriesInView(Calendar.THURSDAY);
+        resetAllButtons();
+        Button thuBtn = findViewById(R.id.thursdayBtn);
+        thuBtn.setBackgroundResource(R.drawable.button_active);
+        thuBtn.setTextColor(Color.WHITE);
+        thuBtn.setElevation(100);
     }
 
     public void onFriClick(View view) {
         setDeliveriesInView(Calendar.FRIDAY);
+        resetAllButtons();
+        Button friBtn = findViewById(R.id.fridayBtn);
+        friBtn.setBackgroundResource(R.drawable.button_active);
+        friBtn.setTextColor(Color.WHITE);
+        friBtn.setElevation(100);
     }
 
     private void setTodayButton() {
@@ -79,6 +113,9 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         todayBtn.setText(R.string.today);
+        todayBtn.setBackgroundResource(R.drawable.button_active);
+        todayBtn.setElevation(100);
+        todayBtn.setTextColor(Color.WHITE);
     }
 
     private String readDelivieriesJSON() {
@@ -134,7 +171,7 @@ public class MainActivity extends AppCompatActivity {
                 for (int i = 0; i < deliveries.size(); i++) {
                     Model.Delivery delivery = deliveries.get(i);
                     TransitionManager.beginDelayedTransition(parentLayout);
-                    getLayoutInflater().inflate(R.layout.delivery, parentLayout);
+                    ViewGroup view = (ViewGroup) getLayoutInflater().inflate(R.layout.delivery, parentLayout);
                     ConstraintLayout myView = (ConstraintLayout) parentLayout.getChildAt(i);
 
                     ImageView restIcon = (ImageView) myView.findViewById(R.id.restIcon);
@@ -143,16 +180,56 @@ public class MainActivity extends AppCompatActivity {
                     TextView deliveryTime = (TextView) myView.findViewById(R.id.delivTime);
                     TextView deliveryStatus = (TextView) myView.findViewById(R.id.orderStatus);
 
+                    DateFormat outputFormat = new SimpleDateFormat("hh:mm aa");
+                    DateFormat inputFormat = new SimpleDateFormat("HH:mm:ss");
+                    try {
+                        Date dateCutoff = inputFormat.parse(delivery.cutoff);
+                        Date dateDropoff = inputFormat.parse(delivery.dropoff);
+
+                        String sDateCutoff = outputFormat.format(dateCutoff);
+                        String sDateDropoff = outputFormat.format(dateDropoff);
+
+                        orderBy.setText(sDateCutoff);
+                        deliveryTime.setText(sDateDropoff);
+
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+
                     Picasso.with(this).load(delivery.logoUrl).resize(500,400).into(restIcon);
                     restName.setText(delivery.restaurantName);
-                    orderBy.setText(delivery.cutoff);
-                    deliveryTime.setText(delivery.dropoff);
-                    //todo implement delivery status based on boolean values in delivery
+
+                    if (delivery.isOrderPlaced) {
+                        deliveryStatus.setText(R.string.order_placed);
+                    }
+                    else if (delivery.soldOut) {
+                        deliveryStatus.setText(R.string.sold_out);
+                    }
+                    else if (delivery.isPastCutoff) {
+                        deliveryStatus.setText(R.string.is_past_cutoff);
+                    }
+                    else if (delivery.sellingOut) {
+                        deliveryStatus.setText(R.string.order_placed);
+                    }
+                    else {
+                        deliveryStatus.setVisibility(View.INVISIBLE);
+                    }
 
                 }
                 break;
             }
         }
+    }
+
+    private void resetAllButtons() {
+        LinearLayout buttonContainer = findViewById(R.id.buttonContainer);
+        for (int i = 0; i < buttonContainer.getChildCount(); i++) {
+            Button button = (Button) buttonContainer.getChildAt(i);
+            button.setBackgroundResource(R.drawable.button_focus);
+            button.setTextColor(Color.BLACK);
+            button.setElevation(0);
+        }
+
     }
 
 
